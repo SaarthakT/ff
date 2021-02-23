e7f3a002186108f4bbf024e465c36b875aa5446c
SaarthakT <76083818+SaarthakT@users.noreply.github.com>
2021-02-22 19:27:12 -0700

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,11 @@
+class Bob{
+  constructor(x,y){
+    super(x,y,50,50);
+ //   this.image = loadImage("what");
+  }
+  display(){
+    this.body.position.x = mouseX;
+    this.body.position.y = mouseY;
+    super.display();
+  }
+}
